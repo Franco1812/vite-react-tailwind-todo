f1369f60effba035fa29261e31a4995c323f41d8
Franco <fpagadios@gmail.com>
2024-02-06 05:25:52 -0300

    comit nosecuanto

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ const filteredTodos =() => {
 
 
   return (
-    <div className="min-h-screen  bg-gray-300   bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] transition-all duration-1000
+    <div className="min-h-screen  bg-gray-500   bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] transition-all duration-1000
     md:bg-[url('./assets/images/bg-desktop-light.jpg')] md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')] ">
       <Header />
       <main className="container mx-auto px-4 mt-8 md:max-w-xl " >
